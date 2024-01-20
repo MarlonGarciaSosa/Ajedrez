66106378eee7baa8d4d2a2e7b475c1067fe2647a
Marlon <155335960+MarlonGarciaSosa@users.noreply.github.com>
2024-01-20 15:28:48 -0500

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,7 +1,8 @@
+//Cambio de color que realiza el sol y la luna
 function CambiarColorSol(){
     const XD = document.querySelector(".CambiarColor")  
     XD.style.color = "white"
-    const sol = document.querySelector(".sol")
+    const sol = document.querySelector(".header__section__img")
     sol.setAttribute("src", "luna.png")
     sol.setAttribute("onclick", "CambiarColorLuna()")
     
@@ -9,7 +10,7 @@ function CambiarColorSol(){
 function CambiarColorLuna(){
      const XD = document.querySelector(".CambiarColor")
      XD.style.color = "black"
-     const sol = document.querySelector(".sol")
+     const sol = document.querySelector(".header__section__img")
      sol.setAttribute("src", "sol.png")
      sol.setAttribute("onclick", "CambiarColorSol()")
  }
